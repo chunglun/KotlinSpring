75ebf8b7df716e5349ce30646baa18c9da6eff9c
Chung-Lun LU <chunglunlu@gmail.com>
2022-04-07 11:05:45 +0800

    Change controller name

--- src/main/kotlin/com/clluv/poc/controller/NftController.kt
+++ src/main/kotlin/com/clluv/poc/controller/NftController.kt
@@ -8,37 +8,37 @@ import org.springframework.web.bind.annotation.*
 
 @RequestMapping("/nfts")
 @RestController
-class HtmlController {
+class NftController {
 
     @Value("\${company_name}")
     private lateinit var name: String
 
-    private var NFTs = mutableListOf(
-        NFT(1, "CryptoPunks", 100.0),
-        NFT(2, "Sneaky Vampire Syndicate", 36.9),
-        NFT(3, "The Sevens (Official)", 0.6),
-        NFT(4, "Art Blocks Curated", 1.1),
-        NFT(5, "Pudgy Penguins", 2.5),
+    private var nfts = mutableListOf(
+        NFT(1, "nft1", 100.0),
+        NFT(2, "nft2", 36.9),
+        NFT(3, "nft3", 0.6),
+        NFT(4, "nft4", 1.1),
+        NFT(5, "nft5", 2.5),
     )
 
     @GetMapping("/hello")
     fun sayHello(): String = "Hello World"
 
     @GetMapping("")
-    fun getNFTs() = NFTs
+    fun getNFTs() = nfts
 
     @PostMapping("")
     @ResponseStatus(HttpStatus.CREATED)
     fun postNFT(@RequestBody nft: NFT): NFT {
-        val maxId = NFTs.map { it.id }.maxOrNull() ?: 0
+        val maxId = nfts.maxOfOrNull { it.id } ?: 0
         val nextId = maxId + 1
         val newNft = NFT(id = nextId, name = nft.name, floor_price = nft.floor_price)
-        NFTs.add(newNft)
+        nfts.add(newNft)
         return newNft
     }
 
     @GetMapping("/{id}")
-    fun getNFTById(@PathVariable id: Int): NFT? = NFTs.firstOrNull { it.id == id } ?: throw NFTNotFoundException()
+    fun getNFTById(@PathVariable id: Int): NFT? = nfts.firstOrNull { it.id == id } ?: throw NFTNotFoundException()
 
     @GetMapping("/home")
     fun getHome() = "$name: NFTs Marketplace"
--- src/test/kotlin/com/clluv/poc/KotlinSpringApplicationTests.kt
+++ src/test/kotlin/com/clluv/poc/KotlinSpringApplicationTests.kt
@@ -1,13 +1,66 @@
 package com.clluv.poc
 
+import com.clluv.poc.model.NFT
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.junit.jupiter.api.Test
+import org.mockito.internal.matchers.GreaterThan
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
+import org.springframework.test.web.servlet.post
 
+/*
+ * MockMvc --> send requests to the controller
+ * ObjectMapper --> convert an object in Kotlin to a JSON string
+ * */
 @SpringBootTest
-class KotlinSpringApplicationTests {
+@AutoConfigureMockMvc
+class KotlinSpringApplicationTests(
+	@Autowired val mockMvc: MockMvc, @Autowired val objectMapper: ObjectMapper
+) {
 
 	@Test
-	fun contextLoads() {
+	fun `Assert NFTs has nft1 as the first item`() {
+		mockMvc.get("/nfts")
+			.andExpect {
+				status { isOk() }
+				content { contentType(MediaType.APPLICATION_JSON) }
+				jsonPath("$[0].id") { value(1) }
+				jsonPath("$[0].name") { value("nft1") }
+				jsonPath("$[0].floor_price") { value(100) }
+				jsonPath("$.length()") { GreaterThan(1) }
+			}
 	}
 
+	@Test
+	fun `Assert that we can create an NFT`() {
+		mockMvc.get("/nfts/6")
+			.andExpect {
+				status { isNotFound() }
+			}
+
+		val newNFT = NFT(0, "nft6", 77.6)
+		mockMvc.post("/nfts") {
+			contentType = MediaType.APPLICATION_JSON
+			content = objectMapper.writeValueAsString(newNFT)
+		}.andExpect {
+			status { isCreated() }
+			content { contentType(MediaType.APPLICATION_JSON) }
+			jsonPath("$.name") { value("nft6") }
+			jsonPath("$.floor_price") { value(77.6) }
+			jsonPath("$.id") { value(6) }
+		}
+
+		mockMvc.get("/nfts/6")
+			.andExpect {
+				status { isOk() }
+				content { contentType(MediaType.APPLICATION_JSON) }
+				jsonPath("$.name") { value("nft6") }
+				jsonPath("$.floor_price") { value(77.6) }
+				jsonPath("$.id") { value(6) }
+			}
+	}
 }
